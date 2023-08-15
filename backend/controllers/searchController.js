f9e9b13dd65b56ea0710d4a75fc8768a78c714c1
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2023-08-15 16:13:03 +0200

    installed axios

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,15 +1,19 @@
 const express = require("express");
+const axios = require("axios");
 
 // Define the searchController function, which takes in a request (req) and a response (res) object.
 exports.searchController = (req, res) => {
-    // Fetch data from the iTunes Search API using the provided query parameters.
-    fetch(
-        `https://itunes.apple.com/search?term=${req.query.name}&limit=30&entity=${req.query.type}`
-    )
-    .then((response) => response.json()) // Convert the response to JSON
-    .then((results) => {
+    // Use Axios to fetch data from the iTunes Search API using the provided query parameters.
+    axios.get(`https://itunes.apple.com/search`, {
+        params: {
+            term: req.query.name,
+            limit: 30,
+            entity: req.query.type
+        }
+    })
+    .then((response) => {
         // Send the fetched results as the response.
-        res.send(results);
+        res.send(response.data);
     })
     .catch((error) => {
         // Handle any errors that occur during the fetch or processing.
