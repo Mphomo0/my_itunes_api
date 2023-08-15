c8f5b84181a91cf5d4b7c73697cc98b6e531b4ba
mpho.moipolai <mpho.moipolai@autotrader.co.za>
2023-08-15 14:28:08 +0200

    change term to name

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -43,7 +43,7 @@ const Search = () => {
     e.preventDefault();
 
     axios
-      .get(`https://mpho-search-api.onrender.com/search/?term=${name}&type=${type}`, {
+      .get(`https://mpho-search-api.onrender.com/search/?name=${name}&type=${type}`, {
         headers: {
           "Content-Type": "application/json"
         },
